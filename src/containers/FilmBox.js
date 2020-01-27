8d289b08b4192e7fb1ecc14e74dabb90f12650c5
Iain <iain2eyes@gmail.com>
2020-01-27 15:40:32 +0000

    Initial setup

---
+++ src/containers/FilmBox.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class FilmBox extends Component {
+
+
+    render() {
+        return(
+            <div>Hello2</div>
+        )
+    }
+
+}
+
+export default FilmBox;
